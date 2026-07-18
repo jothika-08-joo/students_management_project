16a9f9cdafa71cdd8de54b08e58f3c8b2acd5af7
JOTHIKA <jothikakumaravadivel@gmail.com>
2026-07-18 16:55:06 +0530

    create get route

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 const express=require("express");
 const app=express();
-app.listen(3000);
+
+app.get("/",(req,res)=>{
+    res.send("hello world");
+});
+
+app.get("/students",(req,res)=>{
+    res.json([
+    {
+        name:"joo",
+        id:1
+    },
+    {
+       name:"a",
+       id:2
+    },
+    ]);
+});
+
+app.listen(3000,()=>{
+    console.log("server running on port 3000");
+});
+
+

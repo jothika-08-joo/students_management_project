b99815ab5514a72f558b93ee35cf0e46c7e1e466
JOTHIKA <jothikakumaravadivel@gmail.com>
2026-07-22 17:45:29 +0530

    implemented crud api with postgresql

--- app.js
+++ app.js
@@ -1,75 +1,109 @@
 const express=require("express");
 const app=express();
 app.use(express.json());
+const pool=require("./db");
 
-let students=[
-    {
-        id:1,
-        name:"joo"
-    },
-    {
-        id:2,
-        name:"jothika"
-    }
-];
 
 app.get("/",(req,res)=>{
     res.send("hello world");
 });
 
-app.get("/students",(req,res)=>{
-    res.json(students);
-});
-
-app.get("/students/:id",(req,res)=>{
-    const id=Number(req.params.id);
-    const student=students.find((student) => student.id===id);
-    if(!student){
-        return res.status(404).json({
-               message:"student not found"
-            })
-    };
-    res.json(student);
-})
-
-app.put("/students/:id",(req,res)=>{
-    const id=Number(req.params.id);
-    const student=students.find((student)=> student.id===id);
-    if(!student){
-        return res.status(404).json({
-            message:"student not found"
-        });    
+app.get("/students",async (req,res)=>{
+    try{
+        const result=await pool.query("select * from students");
+        res.json(result.rows);
+    }catch(err){
+        console.error(err)
+        res.status(500).json({
+            message:"database error"
+        });
     }
-    student.name=req.body.name
-    res.status(200).json({
-        message:"student updated successfully"
-    });
-
-})
-
-app.post("/students", (req,res)=>{
-    const new_student={
-        id:students.length +1,
-        name:req.body.name
-    };
-    students.push(new_student);
-    res.status(201).json({
-        message:"student created successfully"
-    });
 });
 
-app.delete("/students/:id",(req,res)=>{
+app.get("/students/:id",async(req,res)=>{
+    try{
     const id=Number(req.params.id);
-    const student=students.find(student=> student.id===id);
-    if(!student){
+    const result=await pool.query("select * from students where id=($1)",
+    [id]
+    );
+    if (result.rows.length===0){
         return res.status(404).json({
             message:"student not found"
         });
-    };
-    students=students.filter(student=> student.id!==id);
-    res.status(200).json({
-        message:"student deleted successfully"
-    });
+    }    
+    res.json(result.rows[0])    
+    } catch (err){
+        console.error(err)
+        res.status(500).json({
+            message:"database error"
+        });
+
+    }
+    
+
+});
+
+app.put("/students/:id",async(req,res)=>{
+    try{
+        const id=Number(req.params.id);
+        const name=req.body.name
+        const  result=await pool.query("update students set name=$1 where id=$2 returning *",
+        [name,id]);
+        if(result.rows.length===0){
+            return res.status(404).json({
+                message:"student not found"
+            });
+        }    
+        res.status(200).json({
+            message:"student updated successfully",
+            result:result.rows[0]
+        });    
+    } catch(err){
+        console.error(err)
+        res.status(500).json({
+            message:"database error"
+        });
+    }
+})
+app.post("/students", async(req,res)=>{
+    try{
+        await pool.query("insert into students (name) values($1)",
+        [req.body.name]);
+        res.status(201).json({
+            message:"student created successfully"
+        });
+    }catch(err){
+        console.error(err);
+        res.status(500).json({
+            message:"database error"
+        });
+    }
+    
+
+});
+
+app.delete("/students/:id",async(req,res)=>{
+    try{
+        const id=Number(req.params.id);
+        const result=await pool.query("delete from students where id=$1 returning *",
+            [id]
+        );
+        if(result.rows.length===0){
+            return res.status(404).json({
+                message:"student not found"
+            });
+        }
+        res.status(200).json({
+            message:"student deleted successfully",
+            result:result.rows[0]
+        })
+    }catch(err){
+        console.error(err)
+        res.status(500).json({
+            message:"database error"
+        });
+    }
+    
 });
 
 app.listen(3000,()=>{
